b052abb55df097038d16bf0e6ba8facac40e40c4
sayantika13banik-svg <sayantika13banik@gmail.com>
2026-04-12 01:02:13 +0530

    Integrated MongoDB with API

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const CaseSchema = new mongoose.Schema({
+  description: String,
+  lat: Number,
+  lng: Number,
+  status: String,
+});
 
+const Case = mongoose.model("Case", CaseSchema);
 const app = express();
+mongoose.connect("mongodb+srv://sayantika13banik_db_user:Ts13%401989@cluster0.s8vypth.mongodb.net/safestray?retryWrites=true&w=majority")
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
@@ -17,18 +28,18 @@ app.listen(5000, () => {
 let cases = [];
 
 // POST - add case
-app.post("/report", (req, res) => {
-  const newCase = {
-    id: Date.now(),
+app.post("/report", async (req, res) => {
+  const newCase = new Case({
     ...req.body,
     status: "Pending",
-  };
+  });
 
-  cases.push(newCase);
+  await newCase.save();
   res.json(newCase);
 });
 
 // GET - all cases
-app.get("/cases", (req, res) => {
+app.get("/cases", async (req, res) => {
+  const cases = await Case.find();
   res.json(cases);
 });
